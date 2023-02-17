a8346660ab2ddaea3ca0bb8008c60f23ebc3645c
Francisco Montilla <ciscomontilla@gmail.com>
2023-02-17 16:31:17 -0400

    fix errors

--- components/RangeField.js
+++ components/RangeField.js
@@ -2,7 +2,7 @@
 export const RangeField = ({ label, value, onChange, helperText, min, max }) => {
     return (
         <div className='flex flex-col items-start gap-1 py-2'>
-            <label className='text-gray-800 text-sm'>{label} ({value})</label>
+            <label className='text-gray-800 text-sm'>{label} <span className="text-xs font-semibold">({value} pixeles)</span></label>
             <span className="h-8 w-full">
                 <input value={value} onChange={onChange} type="range" min={min} max={max} step="2" className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer  range accent-orange-600' />
 
